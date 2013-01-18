9211d9a32d0b174e6f7cdfcf14e68eecdf2a4f1b
Ethan Mick <ethanmski@gmail.com>
2013-01-17 22:58:04 -0500

    Added EMFileManager

---
+++ EMFileManager/EMFileManager/EMFileManager.m
@@ -0,0 +1,13 @@
+//
+//  EMFileManager.m
+//  EMFileManager
+//
+//  Created by Ethan Mick on 1/17/13.
+//  Copyright (c) 2013 Ethan Mick. All rights reserved.
+//
+
+#import "EMFileManager.h"
+
+@implementation EMFileManager
+
+@end
